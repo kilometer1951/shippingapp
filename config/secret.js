032d54e002f2381a08d1b676660ec90a43bf7054
Wilson Serge <sergeshalowilson@gmail.com>
2019-03-28 00:16:44 +0000

    jj

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://root:Louis1951@ds231229.mlab.com:31229/oldsailorsshipping',
+    database: 'mongodb://root:Louis1951@ds231229.mlab.com:31229/oldsailorsshipping!!!!!!!!!!!!!!!!!',
     secret: '12345'
 }
